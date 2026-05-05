b3c29ff233c813fac1bb2caae3216fec4005e287
Emeke John <jomekgroup@gmail.com>
2026-05-05 15:32:12 +0100

    Initial commit: Clean monorepo structure

--- web/app/src/main/java/online/skillskonnect/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package online.skillskonnect.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- web/assets/services-CnG8vdrV.js
+++
@@ -1,1 +0,0 @@
-import{a as s}from"./skillTypes-DSylDt4Q.js";const o=s,t=o;export{t as C};
